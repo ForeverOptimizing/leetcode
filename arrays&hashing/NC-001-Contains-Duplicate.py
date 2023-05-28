c026f6c5101d7ff9838df02a3631416c1669f98e
Robert Lowder <rlowder94@gmail.com>
2023-05-27 22:00:17 -0700

    try again at some point

--- arrays&hashing/NC-001-Contains-Duplicate.py
+++ arrays&hashing/NC-001-Contains-Duplicate.py
@@ -1,4 +1,7 @@
 """
+217. Contains Duplicate
+https://leetcode.com/problems/contains-duplicate/
+
 Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
 
@@ -20,19 +23,144 @@ Constraints:
 """
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution(object):
     def containsDuplicate(self, nums):
-        setnums = set(nums)
-        listnums = list(setnums)
-        return listnums == nums
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
+        hashset = set()
+        for num in nums:
+            if num in hashset:
+                return True
+            hashset.add(num)
+        return False
 
 
 newSolution = Solution()
-exampleList = [1,2,3,4]
+exampleList = [1,1,1,3,3,4,3,2,4,2]
+print(newSolution.containsDuplicate(exampleList))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+# My Solution 5/27/23
+# This works, but it sorts the example list in the end
+# EDIT: This actually fails with negative numbers
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        nums.sort()
+        setnums = set(nums)
+        listnums = list(setnums)
+        return listnums != nums
+
+        # :type nums: List[int]
+        # :rtype: bool
+
+
+
+newSolution = Solution()
+exampleList = [1,3,4,2]
+print(newSolution.containsDuplicate(exampleList))
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# NeetCode Solution
+# https://youtu.be/3OamzN90kPg
+
+# class Solution(object):
+#      def containsDuplicate(self, nums: List[int]) -> bool:
+#             hashset = set()
+
+#             for n in nums:
+#                 if n in hashset:
+#                     return True
+#                 hashset.add(n)
+#             return False
+
+
+
+"""
+First Attempt
+    Upon further inspection it messes up if you have the list out of order without duplicates
+    Possible solution is to sort the list of nums somehow before the comparing the lists
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        
+        setnums = set(nums)
+        listnums = list(setnums)
+        return listnums != nums
+
+        # :type nums: List[int]
+        # :rtype: bool
+
+
+
+newSolution = Solution()
+exampleList = [1,3,4,2]
 print(newSolution.containsDuplicate(exampleList))
 
 
@@ -40,3 +168,5 @@ print(newSolution.containsDuplicate(exampleList))
 # setnums = set(exnums)
 # exnums2 = list(setnums)
 # print(exnums==exnums2)
+
+"""
