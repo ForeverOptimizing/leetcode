0620a775109ea2d3074879adef4025d6a9e5d727
Robert Lowder <rlowder94@gmail.com>
2023-07-11 20:02:30 -0700

    formatting

--- arrays&hashing/NC-002-Valid-Anagram.py
+++ arrays&hashing/NC-002-Valid-Anagram.py
@@ -1,4 +1,7 @@
 """
+242. Valid Anagram - EASY
+https://leetcode.com/problems/valid-anagram/
+
 Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
@@ -32,7 +35,10 @@ TIME LIMIT 30 min
 
 """
 
-
+class Solution(object):
+    def isAnagram(self, s, t):
+        if s.sorted == t.sorted:
+            return 
 
 
 
